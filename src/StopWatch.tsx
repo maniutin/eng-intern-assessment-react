fe8d2494b9424b507407516f3fe3077a389eae13
Nick <maniutin@gmail.com>
2024-01-24 11:06:33 -0500

    implement basic stopwatch

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 export default function StopWatch() {
-    return(
-        <div></div>
-    )
+  const [time, setTime] = useState<number>(0);
+  const [isRunning, setIsRunning] = useState<boolean>(false);
+
+  useEffect(() => {
+    let intervalId: NodeJS.Timeout;
+    if (isRunning) {
+      intervalId = setInterval(() => setTime(time + 1), 10);
+    }
+    return () => clearInterval(intervalId);
+  }, [isRunning, time]);
+
+  const hours = Math.floor(time / 360000);
+  const minutes = Math.floor((time % 360000) / 6000);
+  const seconds = Math.floor((time % 6000) / 100);
+  const milliseconds = time % 100;
+
+  const startStop = () => {
+    setIsRunning(!isRunning);
+  };
+
+  return (
+    <div>
+      <div>
+        {hours}:{minutes.toString().padStart(2, "0")}:
+        {seconds.toString().padStart(2, "0")}:
+        {milliseconds.toString().padStart(2, "0")}
+      </div>
+      <div>
+        <button onClick={startStop}>{isRunning ? "Stop" : "Start"}</button>
+      </div>
+    </div>
+  );
 }
