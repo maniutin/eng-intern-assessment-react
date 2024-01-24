4130c0daadde1ac8b6779d71ff46b6f509513a6f
Nick <maniutin@gmail.com>
2024-01-24 11:10:00 -0500

    add reset button

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,24 +1,35 @@
 import React, { useEffect, useState } from "react";
 
 export default function StopWatch() {
+  // store time
   const [time, setTime] = useState<number>(0);
+
+  // check if the timer is running
   const [isRunning, setIsRunning] = useState<boolean>(false);
 
   useEffect(() => {
     let intervalId: NodeJS.Timeout;
     if (isRunning) {
+      // setting time
       intervalId = setInterval(() => setTime(time + 1), 10);
     }
     return () => clearInterval(intervalId);
   }, [isRunning, time]);
 
+  // calculate time values
   const hours = Math.floor(time / 360000);
   const minutes = Math.floor((time % 360000) / 6000);
   const seconds = Math.floor((time % 6000) / 100);
   const milliseconds = time % 100;
 
+  // start/stop the timer
   const startStop = () => {
     setIsRunning(!isRunning);
+  };
+
+  // reset timer to 0
+  const reset = () => {
+    setTime(0);
   };
 
   return (
@@ -30,6 +41,7 @@ export default function StopWatch() {
       </div>
       <div>
         <button onClick={startStop}>{isRunning ? "Stop" : "Start"}</button>
+        <button onClick={reset}>Reset</button>
       </div>
     </div>
   );
